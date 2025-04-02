cce8eea16315376cb5bbfdc8fd0ae946ab8beef4
Ailish <snickrrdoodles@gmail.com>
2025-04-02 07:53:17 -0400

    Added login functionality, created user home page

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request, redirect
 from sqlalchemy import create_engine, text
 
 app = Flask(__name__)
@@ -25,15 +25,33 @@ def signup():
             conn.execute(text("INSERT INTO USERS (fname, lname, ssn, address, phone, username, password) VALUES (:fname, :lname, :ssn, :address, :phone, :username, :password)"),
                         {"fname": fname, "lname": lname, "ssn": ssn, "address": address, "phone": phone, "username": username, "password": password})
             conn.commit()
-            message = "Sign up successful."
+            message = "Sign up successful. Please wait for an admin's approval to log in."
         except:
             message = "ERROR: Sign up failed. Could not create user."
     return render_template("signup.html", message=message)
 
-@app.route("/login")
+@app.route("/login", methods = ["GET", "POST"])
 def login():
     message = None
+    if request.method == "POST":
+        username = request.form.get("username")
+        password = request.form.get("password")
+        try:
+            login = conn.execute(text("SELECT * FROM users WHERE username = :username AND password = :password"),
+                        {"username": username, "password": password})
+            if login:
+                return redirect('/home')
+            else:
+                message = "ERROR: Login failed."
+        except:
+            message = "ERROR: Login failed."
     return render_template("login.html", message=message)
+
+@app.route("/home")
+def user_home():
+    message = None
+    return render_template("user_home.html", message=message)
+
 
 if __name__ == "__main__":
     app.run(debug=True)
